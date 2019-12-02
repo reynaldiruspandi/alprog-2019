48e15294330053b14ae8d405b516fdbc4924fd1f
Reynaldi Ruspandi <ruspandireynaldi@gmail.com>
2019-12-02 08:06:43 +0800

    buat tugas 3

---
+++ tugas3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int bilangan[] = {10,5,1,3,5};
+	cout<<"Soal no.2 \n";
+	cout<<"\n"<<bilangan[0];
+	cout<<"\n"<<bilangan[1];
+	cout<<"\n"<<bilangan[2];
+	cout<<"\n"<<bilangan[3];
+	cout<<"\n"<<bilangan[4]<<"\n";
+}
